4bee9d17b427ec5ad701f756a97f2e644d79c9f0
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-10-01 13:32:35 +0200

    first example

---
+++ script.r
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+dati <- read.csv('./data/Trieste_2022.csv',  
+                skip = 2, 
+                encoding = "UTF-8",
+                quote = "\"")
+
+colnames(dati)
+nrow(dati)
+ncol(dati)
+str(dati)
+dati_solo_trieste <- dati %>%
+    filter(Denominazione == "Trieste")%>%
+    select(Età, Totale.Maschi, Totale.Femmine)%>%
+    mutate(eta = as.numeric(Età))%>%
+    filter(eta<=100)%>%
+    select(-Età)
+
+colnames(dati_solo_trieste)
+nrow(dati_solo_trieste)
+ncol(dati_solo_trieste)
+str(dati_solo_trieste)
+
+com_trieste_22 <- dati_solo_trieste %>%
+    mutate(popolazione = Totale.Maschi + Totale.Femmine) %>%
+    mutate(anno=2022)%>%
+    select(anno,eta,popolazione)
+
+colnames(com_trieste_22)
+nrow(com_trieste_22)
+ncol(com_trieste_22)
+str(com_trieste_22)
+
+write.csv (com_trieste_22, "./results/com_trieste_2022.csv", 
+    row.names=FALSE,
+    fileEncoding = "UTF-8",
+    quote = FALSE )
+
+
+
+ggplot(com_trieste_22, aes(x=eta, y=popolazione))+
+    geom_col( fill="blue", color="white")+
+    coord_flip () + 
+    theme_minimal() + 
+    ggtitle("Popolazione residente nel comune di Trieste al 1° gennaio 2022")
+
+ggsave('./results/grafico.png') 
